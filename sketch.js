44a7ede940c8fa9ccd1583e2ca4936eb329e0484
jiana2107 <71509223+jiana2107@users.noreply.github.com>
2020-10-08 15:15:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup() {
   thickness=random(22,83);
 
   bullet= createSprite(50, 200, 20, 50);
-  bullet.shapeColor=color("white");
+  bullet.shapeColor=color("purple");
   bullet.velocityX=speed;
 
   wall=createSprite(1200,200,thickness,height/2);
