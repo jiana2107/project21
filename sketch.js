240b98a78423d701d363178fac29a3c2bcb96b2b
jiana2107 <71509223+jiana2107@users.noreply.github.com>
2020-10-14 16:54:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,9 +46,9 @@ function hasCollided(bullet,wall){
   bulletRightEdge=bullet.x+bullet.width/2;
   wallLeftEdge=wall.x - wall.width/2;
   
-   if(bulletRightEdge>=wallLeftEdge)
-  { return true; } 
-   else{ 
+   if(bulletRightEdge>=wallLeftEdge){
+   return true; 
+   }else{ 
     return false; }
   
 if(hasCollided(bullet,wall)){
